d4fab2600dae0faabfc391265ca431156658939b
martcode123 <martrich205@gmail.com>
2022-12-17 17:24:02 +0000

    trailer modal completed

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,5 +1,7 @@
 import Image from "next/legacy/image";
+import { modalState, movieState } from "../pages/atoms/modalAtom";
 import { Movie } from "../typings";
+import { useRecoilState } from "recoil"
 
 interface Props {
   movie: Movie;
@@ -7,9 +9,12 @@ interface Props {
 }
 
 function Thumbnail({ movie }: Props) {
+
+    const [showModal, setShowModal] = useRecoilState(modalState)
+    const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
-      
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ">
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
@@ -17,8 +22,14 @@ function Thumbnail({ movie }: Props) {
         alt=""
         className="rounded-sm object-cover md:rounded"
         layout="fill"
+        onClick={() => {
+          setCurrentMovie(movie);
+          setShowModal(true);
+        }}
       />
-    
+      {/* <h1 className="pt-9  absolute text-1xl text-center  md:text-2xl lg:text-2xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1> */}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import requests from "../utills/requests";
 import { Movie } from "../typings";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
+import { useRecoilValue } from "recoil";
+import { modalState } from "./atoms/modalAtom";
+import Modal from "../components/Modal";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -28,7 +31,8 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  const { logout, loading } = useAuth()
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
 
   if(loading) return null
   return (
@@ -53,6 +57,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
+     {showModal && <Modal />}
     </div>
   );
 };
